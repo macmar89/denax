41bd8611a9b1c329b1a66a0722d187742bbce967
Marian <marian.karola@gmail.com>
2021-08-17 23:35:06 +0200

    task three -> hoc

--- src/pages/TaskThree.jsx
+++ src/pages/TaskThree.jsx
@@ -1,86 +1,16 @@
-import React, { useState, useMemo, useContext } from "react";
-import { NumberOfSeats, RowsContext } from "../App";
+import React from "react";
 import CountOfChoosenSeats from "../components/global/CountOfChoosenSeats";
 import Header from "../components/global/Header";
+import HighComponent from "../components/global/HighComponent";
 
-const TaskThree = () => {
-  const rows = useContext(RowsContext);
-  const numberOfSeatsInRow = useContext(NumberOfSeats);
-
-  const numberOfSeats = rows * numberOfSeatsInRow;
-
-  const [seat, setSeat] = useState([]);
-
-  // vyrenderuje sedadla s ID a stavom checknutia (false)
-  useMemo(() => {
-    console.log("tu sa robi renderSeats");
-    let seats = [];
-    for (let i = 1; i <= numberOfSeats; i++) {
-      seats.push({ id: i, checked: false });
-    }
-    setSeat(seats);
-  }, [numberOfSeats]);
-
-  //  cisla radov
-  let rowsNumbers = [];
-  for (let i = 1; i <= rows; i++) {
-    rowsNumbers.push(i);
-  }
-
-  //  cisla sedadiel
-  let seatsNumbers = [];
-  for (let i = 1; i <= numberOfSeatsInRow; i++) {
-    seatsNumbers.push(i);
-  }
-
-  // vyberie oznacene sedadlo / takisto ho aj vymaze ak je uz oznacene
-  const chooseSeat = num => {
-    let newSeat = [...seat];
-    newSeat[num - 1].checked = !newSeat[num - 1].checked;
-    setSeat(newSeat);
-  };
-
-  //  vybrane miesta
-  const choosenSeats = seat.filter(s => s.checked === true);
-
-  //  urci miesto v hladisku
-  const urciMiesto = seat => {
-    function isInt(n) {
-      return n % 1 === 0;
-    }
-    const rad = isInt(seat / numberOfSeatsInRow)
-      ? seat / numberOfSeatsInRow
-      : Math.ceil(seat / numberOfSeatsInRow);
-
-    const miesto = numberOfSeatsInRow + (seat - rad * numberOfSeatsInRow);
-
-    return `rad ${rad}, sedadlo: ${miesto}`;
-  };
-
-  const renderRow = numberOfRow => {
-    const sliced = seat.slice(
-      numberOfRow * numberOfSeatsInRow - numberOfSeatsInRow,
-      numberOfRow * numberOfSeatsInRow
-    );
-
-    return (
-      <tr>
-        <td>{`Rad ${numberOfRow}`}</td>
-        {sliced.map(num => (
-          <td key={num}>
-            <input
-              type="checkbox"
-              onChange={e => chooseSeat(+e.target.name)}
-              checked={num.checked}
-              className="form-check-label mx-1"
-              name={num.id}
-            />
-          </td>
-        ))}
-      </tr>
-    );
-  };
-
+const TaskThree = ({
+  choosenSeats,
+  renderRow,
+  numOfRows,
+  numOfSeatsInRow,
+  urciMiesto,
+  chooseSeat,
+}) => {
   return (
     <div className="container">
       <Header title="Úloha 3" />
@@ -90,14 +20,14 @@ const TaskThree = () => {
             <thead>
               <tr>
                 <td>-</td>
-                {seatsNumbers.map(num => (
+                {numOfSeatsInRow.map(num => (
                   <th key={num} className="text-center">
                     {num}
                   </th>
                 ))}
               </tr>
             </thead>
-            <tbody>{rowsNumbers.map(num => renderRow(num))}</tbody>
+            <tbody>{numOfRows.map(num => renderRow(num))}</tbody>
           </table>
           <CountOfChoosenSeats count={choosenSeats.length} />
         </div>
@@ -123,4 +53,4 @@ const TaskThree = () => {
   );
 };
 
-export default TaskThree;
+export default HighComponent(TaskThree, true);
